2630a2074c0ce86c8507f2ff28dd96c9bd493754
gauravcg492 <officialgauravcg492@gmail.com>
2020-04-12 19:11:05 +0530

    Almost all backend api added, testing left

--- backend/api/models/mail.js
+++ backend/api/models/mail.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
+
+// initialize the incrementor
+autoIncrement.initialize(mongoose.connection);
 
 // defining mail schema
 const mailSchema = mongoose.Schema({
+    mailID: Number,
     sender: {
         type: String,
         required: true
@@ -32,6 +37,13 @@ const mailSchema = mongoose.Schema({
         }
     }
     // TODO attachments
+});
+
+mailSchema.plugin(autoIncrement.plugin,{
+    model : 'Mail',
+    field : 'mailId',
+    startAt : 10000,
+    incrementBy : 1 
 });
 
 module.exports = mongoose.model('Mail',mailSchema);
--- backend/api/routes/mails.js
+++ backend/api/routes/mails.js
@@ -1,7 +1,6 @@
 // dependencies
 const express = require('express');
 const request = require('request-promise');
-const mongoose = require('mongoose');
 const helper = require('../helper');
 const authenticate = require('../middlewares/authenticate');
 const Mail = require('../models/mail');
@@ -10,23 +9,101 @@ const Mail = require('../models/mail');
 const router = express.Router();
 
 // to add mail
-router.post('/post', authenticate, (req,res,next) =>{
+// receive { receivers=[] , subject=str, body=str  }
+router.post('/post', authenticate, async (req, res, next) => {
+    const sender = req.decodedToken.user;
+    try {
+        const category = JSON.parse(await request.get('')).category;
+        const mail = new Mail({
+            sender: sender,
+            receiver: req.body.receiver,
+            timeSent: new Date(),
+            subject: req.body.subject,
+            body: req.body.body,
+            category: 5 //TODO get category based on the api call
+        });
+        await mail.save();
+        res.status(201).json();
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({});
+    }
 
 });
 
 // to send all sent mails
-router.get('/sent', authenticate, (req,res,next) => {
-
+router.get('/sent', authenticate, async (req, res, next) => {
+    const sender = req.decodedToken.user;
+    try {
+        const mails = Mail.find({ sender: sender });
+        console.log(mails);
+        if (mails.length >= 1) {
+            res.status(200).json(mails);
+        } else {
+            res.status(200).json({});
+        }
+    } catch(err){
+        console.log(err);
+        res.status(500).json();
+    } 
 });
 
 // to send all received mails
-router.get('/received', authenticate, (req,res,next) => {
+router.get('/received', authenticate, async (req, res, next) => {
     const category = req.query.category;
+    const receiver = req.decodedToken.user;
+    try {
+        const mails = Mail.find({ receiver: receiver, category: category });
+        console.log(mails);
+        if (mails.length >= 1) {
+            res.status(200).json(mails);
+        } else {
+            res.status(200).json({});
+        }
+    } catch(err){
+        console.log(err);
+        return res.status(500).json();
+    } 
+});
+
+// to save that particular user read the mail
+router.post('/read', authenticate, (req,res,next) => {
+    const mailId = req.body.mailId;
+    const reader = req.decodedToken.user;
+    try{
+        const response = await Mail.updateOne({mailId: mailId},{ $addToSet: {readBy: reader}});
+        console.log(response);
+        if(response.nModified == 0){
+            res.status(500).json();
+        } else{
+            res.status(200).json();
+        }
+    } catch(err){
+        console.log(err);
+        res.status(500).json();
+    }
 });
 
 // to delete mail
-router.delete('/:mailId', authenticate, (req,res,next) => {
+router.delete('/:mailId', authenticate, async (req, res, next) => {
     const mailId = req.params.mailId;
+    const user = req.decodedToken.user;
+
+    try{
+        response = await Mail.find({mailId: mailId, sender: user});
+        if(response.length >= 1){
+            await Mail.remove({mailId: mailId});
+        } else{
+            response = await Mail.find({mailId: mailId, receiver: user});
+            if(response.length >= 1){
+                await Mail.updateOne({mailId: mailId}, {$pull: {receiver: user}});
+            }
+        }
+        res.status(200).json();
+    } catch(err){
+        console.log(err);
+        res.status(500).json();
+    }
 });
 
 
